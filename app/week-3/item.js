08513b24d9a79ee46403ceceb90098efbb6736da
Kenny <prizekenny@gmail.com>
2024-09-19 13:49:04 -0600

    Change style of week 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="list-none bg-blue-950 text-white w-64 m-3 p-2">
+    <li className="list-none bg-blue-100 w-80 m-3 p-2">
       <p className="text-2xl font-bold">{name}</p>
       <p>
         Buy {quantity} in {category}
